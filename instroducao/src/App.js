94734186b3431c992d63138c8c3d406080e88d94
Laura Lobo <lauralobo364@gmail.com>
2024-03-11 11:20:11 -0300

    06Estados

--- instroducao/src/App.js
+++ instroducao/src/App.js
@@ -1,12 +1,17 @@
 // import SistemaSolar from "./components/04Children/SistemaSolar"
 // import Planeta from "./components/04Children/Planeta"
-import Galaxia from "./components/04Children/Galaxia"
+//import Galaxia from "./components/04Children/Galaxia"
+//import ComPai from "./components/05ComPaiFilho/ComPai"
+//import Estado from "./components/06Estados/Estado"
+import Pokemon from "./components/06Estados/Pokemon";
+
 function App() {
-  return  (
-    <div>
-      
-    </div>
-    
-  )
+    return (
+        <div>
+            <h1>Desenvolvimento de Software para Web!</h1>
+            <Pokemon />
+        </div>
+    );
 }
-export default App
+
+export default App;
